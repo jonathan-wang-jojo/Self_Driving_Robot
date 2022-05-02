0f3c97e4258e418016dcc70711d82c4979ea8932
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-05-02 10:31:01 -0400

    Update main3.py

--- main3.py
+++ main3.py
@@ -91,7 +91,7 @@ def reader():
         return retur
 
 def livestream(frame):
-    decoded = cv.imdecode(np.frombuffer(frame, dtype=np.uint8),-1)
+    decoded = cv.imdecode(np.frombuffer(frame, dtype=np.uint8))
     #decoded = decoded.reshape((480, 640, 3))
     decoded= cv.cvtColor(decoded, cv.COLOR_RGB2BGR)
     process(decoded)
