6fc53a28a935af1ec82a25794ff6fce2e825a68d
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-05-02 12:09:32 -0400

    Update main4.py

--- main4.py
+++ main4.py
@@ -8,78 +8,80 @@ import logging
 
 def process(img):
     grayed = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    dst = cv.Canny(grayed, 200, 400, None, 3)
+    dst = cv.Canny(grayed, 50, 50, None, 3)
     blurred = cv.blur(dst, (20, 20))
     height = img.shape[0] - 1
     width = img.shape[1] - 1
-    slice_height = height // 16
+    slice_height = height // 20
     midpoints = []
-    thresh = 20
+    thresh = 10
     im_bw = cv.threshold(blurred, thresh, 255, cv.THRESH_BINARY)[1]
-    for j in range(15):
-        y = height - j * slice_height
-        check1 = False
-        check2 = False
-        for x in range(width):
-            if (im_bw[y, x] > 40):
-                startx = x
-                check1 = True
+    try:
+        for j in range(19):
+            y = height - j * slice_height
+            check1 = False
+            check2 = False
+            for x in range(width):
+                if (im_bw[y, x] > 40):
+                    startx = x
+                    check1 = True
+                    break
+            for x in range(width):
+                if im_bw[y, width - x] > 40:
+                    endx = width - x
+                    check2 = True
+                    break
+            if (check1 and check2 and abs(startx - endx) > 100):
+                mid = (startx + endx) // 2
+                midpoints.append((mid, y))
+            else:
+                ending = y
                 break
-        for x in range(width):
-            if im_bw[y, width - x] > 40:
-                endx = width - x
-                check2 = True
-                break
-        if (check1 and check2 and abs(startx - endx) > 100):
-            mid = (startx + endx) // 2
-            midpoints.append((mid, y))
+        second_x = midpoints[-1][0]
+        slope = (midpoints[-1][1] - midpoints[-2][1])
+
+        if (slope < 0):
+            slice_width = (width - midpoints[-1][0]) // 16
+            for i in range(3, 16):
+                x = second_x + slice_width * i
+                check1 = False
+                check2 = False
+                for y in range(height):
+                    if (im_bw[y, x] > 40):
+                        starty = y
+                        check1 = True
+                        break
+                for y in range(height):
+                    if (im_bw[height - y, x] > 40):
+                        endy = height - y
+                        check2 = True
+                        break
+                if (check1 and check2 and abs(starty - endy) > 100):
+                    mid = (starty + endy) // 2
+                    midpoints.append((x, mid))
         else:
-            ending = y
-            break
-
-    second_x = midpoints[-1][0]
-    slope = (midpoints[-1][1] - midpoints[-2][1]) / (midpoints[-1][0] - midpoints[-2][0])
-
-    if (slope < 0):
-        slice_width = (width - midpoints[-1][0]) // 16
-        for i in range(3, 16):
-            x = second_x + slice_width * i
-            check1 = False
-            check2 = False
-            for y in range(height):
-                if (im_bw[y, x] > 40):
-                    starty = y
-                    check1 = True
-                    break
-            for y in range(height):
-                if (im_bw[height - y, x] > 40):
-                    endy = height - y
-                    check2 = True
-                    break
-            if (check1 and check2 and abs(starty - endy) > 100):
-                mid = (starty + endy) // 2
-                midpoints.append((x, mid))
-    else:
-        slice_width = midpoints[-1][0] // 16
-        for i in range(3, 16):
-            x = second_x - slice_width * i
-            check1 = False
-            check2 = False
-            for y in range(height):
-                if (im_bw[y, x] > 40):
-                    starty = y
-                    check1 = True
-                    break
-            for y in range(height):
-                if (im_bw[height - y, x] > 40):
-                    endy = height - y
-                    check2 = True
-                    break
-            if (check1 and check2 and abs(starty - endy) > 100):
-                mid = (starty + endy) // 2
-                midpoints.append((x, mid))
-    for i in range(len(midpoints) - 1):
-        cv.arrowedLine(img=img, pt1=midpoints[i], pt2=midpoints[i + 1], thickness=5, color=(0, 255, 0))
+            slice_width = midpoints[-1][0] // 16
+            for i in range(3, 16):
+                x = second_x - slice_width * i
+                check1 = False
+                check2 = False
+                for y in range(height):
+                    if (im_bw[y, x] > 40):
+                        starty = y
+                        check1 = True
+                        break
+                for y in range(height):
+                    if (im_bw[height - y, x] > 40):
+                        endy = height - y
+                        check2 = True
+                        break
+                if (check1 and check2 and abs(starty - endy) > 100):
+                    mid = (starty + endy) // 2
+                    midpoints.append((x, mid))
+        for i in range(len(midpoints) - 1):
+            cv.arrowedLine(img=img, pt1=midpoints[i], pt2=midpoints[i + 1], thickness=5, color=(0, 255, 0))
+    except Exception:
+        img = img
     data= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return bytes(data)
 
@@ -92,8 +94,7 @@ def reader():
         return retur
 
 def livestream(frame):
-    decoded = cv2.imdecode(np.frombuffer(frame, dtype=np.uint8))
-    #decoded = decoded.reshape((480, 640, 3))
+    decoded = cv2.imdecode(np.frombuffer(frame, dtype=np.uint8),cv.IMREAD_COLOR)
     decoded= cv.cvtColor(decoded, cv.COLOR_RGB2BGR)
     return process(decoded)
 
