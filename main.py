7a0387dd00fff6fb6a0b4effa77f8e7bf7aaa139
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-05-02 08:15:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ def rest_rev():
     Robert.Reverse(distance)
     return "rev success"
 
-@app.route("/img")
+@app.route("/image")
 def image():
     return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
