ef00f45eedb20fe1d209cc2504abdeb15414e2c2
pl247231 <70585668+pl247231@users.noreply.github.com>
2022-05-18 21:32:10 -0400

    Update main_curve.py

--- main_curve.py
+++ main_curve.py
@@ -6,81 +6,80 @@ from picamera import PiCamera
 from io import BytesIO
 import logging
 
+
 def process(img1):
     try:
-        gray = cv.cvtColor(img1, cv.COLOR_BGR2GRAY)
-        edges = cv.Canny(gray, 100, 200, None, 3)
-        blur = cv.blur(edges, (20, 20))
-        thresh = 20
-        img = cv.threshold(blur, thresh, 255, cv.THRESH_BINARY)[1]
-        height = img.shape[0]-1
-        width = img.shape[1]-1
-        slice_height = height//10
-        slice_width = width//10
+        img = img1.copy()
+        height = img.shape[0] - 1
+        width = img.shape[1] - 1
+        slice_height = height // 10
+        slice_width = width // 10
         h_points = []
         v_points = []
-        img2 = img1.copy()
         for j in range(10):
             bool1 = False
             bool2 = False
-            y = height - slice_height*j
+            y = height - slice_height * j
             for x in range(width):
-                if img[y, x] == 255:
+                if (img[y, x][2] > 100 and img[y,x][1] < 150):
                     x1 = x
                     bool1 = True
                     break
             for x in range(width):
-                if img[y, width - x] == 255:
+                if  img[y, width - x][2] > 100 and img[y,x][1] < 150:
                     x2 = width - x
                     bool2 = True
                     break
-            if (bool1 and bool2) and (abs(x2-x1) > 50):
-                mid = (x1 + x2)//2
+            if (bool1 and bool2) and (abs(x2 - x1) > 50):
+                mid = (x1 + x2) // 2
                 h_points.append((mid, y))
         for j in range(10):
             bool1 = False
             bool2 = False
-            x = slice_width*j
+            x = slice_width * j
             for y in range(height):
-                if img[y, x] == 255:
+                if (img[y, x][2] > 100 and img[y,x][1] < 150):
                     y1 = y
                     bool1 = True
                     break
             for y in range(height):
-                if img[height - y, x] == 255:
+                if img[height - y, x][2] > 100 and img[height - y,x][1] < 150:
                     y2 = height - y
                     bool2 = True
                     break
-            if (bool1 and bool2) and (abs(y2-y1) > 50):
-                mid = (y1 + y2)//2
+            if (bool1 and bool2) and (abs(y2 - y1) > 50):
+                mid = (y1 + y2) // 2
                 v_points.append((x, mid))
-
+        
         slope = (v_points[0][0]-h_points[0][0])
 
         if slope > width/8:
             points = h_points + v_points
-        else if slope < -width/8:
+        elif slope < -width/8:
             v_points.reverse()
             points = h_points + v_points
-
-        for i in range(len(points) - 1):
-            cv.arrowedLine(img = img2, pt1 = points[i], pt2 = points[i+1], thickness = 5, color = (0,255 ,0))
         
-        dh = h_points[0][1]-h_points[1][1]
-        dx = h_points[1][0]-h_points[0][0]
-        angle = np.arctan(dh/dx)
+        for i in range(len(points) - 1):
+            cv.arrowedLine(img = img, pt1 = points[i], pt2 = points[i+1], thickness = 5, color = (0,255 ,0))
+        
+        dh = h_points[0][1] - h_points[1][1]
+        dx = h_points[1][0] - h_points[0][0]
+        angle = np.arctan(dh / dx)
         if angle > 0:
-            if (np.pi/2 - angle > np.pi/36):
-                #right turn
-                Robert.Turn(False, (np.pi/2-angle)*0.82/np.pi)
+            if (np.pi/2 - angle > np.pi / 36):
+                # right turn
+                Robert.Turn(False, (np.pi/2 - angle)*0.82/ np.pi)
         else:
-            #left turn
-            if(np.pi/2 + angle > np.pi/36):
-                Robert.Turn(True, (np.pi/2 + angle)*1.63/np.pi)
+            # left turn
+            if (np.pi / 2 + angle > np.pi / 36):
+                Robert.Turn(True, (np.pi / 2 + angle) * 1.63 / np.pi)
         Robert.Forward(0.1)
+
     except Exception:
         Robert.Forward(0.1)
-    return img2
+    return img
+
+
 def reader():
     retur = ""
     with open("myapp.log", "r") as f:
@@ -88,6 +87,7 @@ def reader():
         for i in range(len(listed) - 3, len(listed)):
             retur += listed[i] + "<br>"
         return retur
+
 
 def gen():
     my_stream = BytesIO()
@@ -99,18 +99,21 @@ def gen():
             my_stream.seek(0)
             my_stream.flush()
             frame = my_stream.getvalue()
-            frame = cv.imdecode(np.frombuffer(frame,np.uint8), cv.IMREAD_COLOR)
+            frame = cv.imdecode(np.frombuffer(frame, np.uint8), cv.IMREAD_COLOR)
             frame = np.hstack((frame, process(frame)))
-            frame = cv.imencode(".jpg", frame)[1] 
+            frame = cv.imencode(".jpg", frame)[1]
             frame = frame.tobytes()
-            yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n'+ frame+ b'\r\n')
+            yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 app = Flask('__name__')
 Robert = Robot()
 
+
 @app.route('/')
 def index():
     return "welcome to the queens"
+
 
 @app.route('/fwd', methods=['GET'])
 def rest_fwd():
@@ -119,15 +122,22 @@ def rest_fwd():
     Robert.Forward(distance)
     return "fwd success"
 
+
 @app.route("/rev", methods=['GET'])
 def rest_rev():
     distance = request.args.get("d", default=1.00, type=float)
     Robert.Reverse(distance)
     return "rev success"
 
+
 @app.route("/image")
 def image():
     return Response(gen(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+
+@app.route("/processed")
+# def processed():
+# return Response(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + list(gen())[1] + b'\r\n', mimetype="multipart/x-mixed-replace; boundary=frame")
 
 @app.route("/left", methods=["GET"])
 def rest_turn():
@@ -135,11 +145,13 @@ def rest_turn():
     Robert.Turn(True, rotation)
     return "turn success"
 
+
 @app.route("/right", methods=["GET"])
 def rest_right():
     rotation = request.args.get("d", default=1.00, type=float)
     Robert.Turn(False, rotation)
     return "turn success"
+
 
 @app.route("/auto")
 def Auto_run():
@@ -155,6 +167,7 @@ def ui():
 @app.route("/term")
 def termin():
     return Response(reader())
+
 
 if __name__ == "__main__":
     logging.basicConfig(filename='myapp.log', level=logging.INFO)
